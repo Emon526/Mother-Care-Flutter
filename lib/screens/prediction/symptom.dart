8e79a76805552c2f9acd5bacdc2240a77e2789d5
Emon526 <emonats526@gmail.com>
2023-01-17 00:28:38 +0600

    updated

--- lib/screens/prediction/symptom.dart
+++ lib/screens/prediction/symptom.dart
@@ -5,14 +5,12 @@ class SymptomPrediction extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Please discribe the symptom to get prediction result',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20.0,
-          ),
+    return const Center(
+      child: Text(
+        'Please discribe the symptom to get prediction result',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20.0,
         ),
       ),
     );
