72bc921569e11a6d54bf1817a2ebac6abfc91262
Emon526 <emonats526@gmail.com>
2023-01-17 23:27:44 +0600

    updated

--- lib/screens/prediction/memmography.dart
+++ lib/screens/prediction/memmography.dart
@@ -1,9 +1,6 @@
-// ignore_for_file: avoid_print
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../../widget/mammogramnote.dart';
@@ -17,37 +14,7 @@ class MemmographyPrediction extends StatefulWidget {
 }
 
 class _MemmographyPredictionState extends State<MemmographyPrediction> {
-  bool _loading = false;
-
-  List? _outputs;
   File? _image;
-
-  @override
-  void initState() {
-    super.initState();
-    _loading = true;
-
-    loadModel().then((value) {
-      setState(() {
-        _loading = false;
-      });
-    });
-  }
-
-  Future<void> loadModel() async {
-    String? res = await Tflite.loadModel(
-      model: "assets/model_unquant.tflite",
-      labels: "assets/labels.txt",
-      // useGpuDelegate: true,
-    );
-    print(res);
-  }
-
-  @override
-  void dispose() {
-    Tflite.close();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,114 +24,107 @@ class _MemmographyPredictionState extends State<MemmographyPrediction> {
         title: const Text('Memmography Prediction'),
         centerTitle: true,
       ),
-      body: _loading
-          ? Container(
-              alignment: Alignment.center,
-              child: const CircularProgressIndicator.adaptive(),
-            )
-          : Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const MammogramNote(),
-                const SizedBox(
-                  height: 10,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const MammogramNote(),
+          const SizedBox(
+            height: 10,
+          ),
+          Column(
+            children: [
+              Container(
+                height: size.width * 0.7,
+                width: size.width * 0.7,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    width: 2.0,
+                    strokeAlign: StrokeAlign.outside,
+                    style: BorderStyle.solid,
+                    color: Theme.of(context).primaryColor,
+                  ),
                 ),
-                Column(
-                  children: [
-                    Container(
-                      height: size.width * 0.7,
-                      width: size.width * 0.7,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.secondary,
+                child: _image == null
+                    ? Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Waiting for Image',
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                          ElevatedButton.icon(
+                            label: const Text('Upload Image'),
+                            onPressed: pickImage,
+                            icon: const Icon(Icons.photo_camera_outlined),
+                          ),
+                        ],
+                      )
+                    : ClipRRect(
                         borderRadius: BorderRadius.circular(20),
-                        border: Border.all(
-                          width: 2.0,
-                          strokeAlign: StrokeAlign.outside,
-                          style: BorderStyle.solid,
-                          color: Theme.of(context).primaryColor,
+                        child: Image.file(
+                          _image!,
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      child: _image == null
-                          ? Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Waiting for Image',
-                                  style: TextStyle(
-                                    color: Theme.of(context).primaryColor,
-                                    fontSize: 18.0,
-                                  ),
-                                ),
-                                ElevatedButton.icon(
-                                  label: const Text('Upload Image'),
-                                  onPressed: pickImage,
-                                  icon: const Icon(Icons.photo_camera_outlined),
-                                ),
-                              ],
-                            )
-                          : ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.file(
-                                _image!,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    _image == null
-                        ? GestureDetector(
-                            onTap: () {
-                              print('What is Mammogram?');
-                            },
-                            child: RichText(
-                              text: const TextSpan(
-                                text: 'What is ',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                ),
-                                children: [
-                                  TextSpan(
-                                      text: 'Mammogram?',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      )),
-                                ],
-                              ),
-                            ),
-                          )
-                        : Column(
-                            children: [
-                              ElevatedButton.icon(
-                                label: const Text('Change Image'),
-                                onPressed: pickImage,
-                                icon: const Icon(Icons.photo_camera_outlined),
-                              ),
-                              ElevatedButton.icon(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => PredictionResult(
-                                        outputs: _outputs,
-                                      ),
-                                    ),
-                                  );
-                                },
-                                icon: const Icon(
-                                  Icons.description_outlined,
-                                ),
-                                label: const Text('Get Prediction Result'),
-                              ),
-                            ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              _image == null
+                  ? GestureDetector(
+                      onTap: () {},
+                      child: RichText(
+                        text: const TextSpan(
+                          text: 'What is ',
+                          style: TextStyle(
+                            color: Colors.black,
                           ),
-                  ],
-                ),
-              ],
-            ),
+                          children: [
+                            TextSpan(
+                                text: 'Mammogram?',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ],
+                        ),
+                      ),
+                    )
+                  : Column(
+                      children: [
+                        ElevatedButton.icon(
+                          label: const Text('Change Image'),
+                          onPressed: pickImage,
+                          icon: const Icon(Icons.photo_camera_outlined),
+                        ),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PredictionResult(
+                                  image: _image!,
+                                ),
+                              ),
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.description_outlined,
+                          ),
+                          label: const Text('Get Prediction Result'),
+                        ),
+                      ],
+                    ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -174,37 +134,14 @@ class _MemmographyPredictionState extends State<MemmographyPrediction> {
       var image = await picker.pickImage(source: ImageSource.gallery);
       if (image == null) return;
       setState(() {
-        _loading = true;
         _image = File(image.path);
       });
-      await classifyImage(_image!);
-      setState(() {
-        _loading = false;
-      });
     } catch (error) {
-      setState(() {
-        _loading = false;
-      });
-      print(error);
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text(error.toString()),
         ),
       );
     }
-  }
-
-  Future<void> classifyImage(File image) async {
-    var output = await Tflite.runModelOnImage(
-      path: image.path,
-      numResults: 2,
-      threshold: 0.5,
-      imageMean: 127.5,
-      imageStd: 127.5,
-    );
-    print('output: $output');
-    setState(() {
-      _outputs = output;
-    });
   }
 }
