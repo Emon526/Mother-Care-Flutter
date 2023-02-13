67eab4ba2237d0f46087b5414de5b7d52b5fbd3c
Emon526 <emonats526@gmail.com>
2023-02-14 03:10:34 +0600

    updated

--- lib/screens/selfcheck/self_check_steps.dart
+++ lib/screens/selfcheck/self_check_steps.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: body_might_complete_normally_nullable
+
 import 'package:flutter/material.dart';
 import 'package:cool_stepper/cool_stepper.dart';
 import 'package:mothercare/screens/selfcheck/self_check_finish.dart';
