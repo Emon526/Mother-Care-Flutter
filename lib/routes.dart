7f27e59dc3b90f1ef092b3d3f733d6c72e6fba99
Emon526 <emonats526@gmail.com>
2023-02-21 04:30:18 +0600

    updated

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-
 import 'screens/doctors/doctorslist.dart';
 import 'screens/memmographyscreening/memmography.dart';
+import 'screens/reminder/reminder.dart';
+import 'screens/reminder/reminderlist.dart';
 import 'widget/persistent_nav_bar.dart';
 
 class RouteManager {
   static const String initialRoute = '/';
   static const String memmographyscreening = '/memmographyscreening';
   static const String doctors = '/doctors';
+  static const String reminderList = '/reminderList';
+  static const String reminder = '/reminder';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -22,6 +25,14 @@ class RouteManager {
       case doctors:
         return MaterialPageRoute(
           builder: (context) => const DoctorsList(),
+        );
+      case reminderList:
+        return MaterialPageRoute(
+          builder: (context) => const ReminderList(),
+        );
+      case reminder:
+        return MaterialPageRoute(
+          builder: (context) => const Reminder(),
         );
       default:
         throw const FormatException("Route Not Found");
