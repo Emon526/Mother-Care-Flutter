2a45a90fdf9025b810d5a1da6f4fe5b3c4bf70a0
Emon526 <emonats526@gmail.com>
2023-03-19 01:30:40 +0600

    pull refresh added

--- lib/widget/emptywidget.dart
+++ lib/widget/emptywidget.dart
@@ -2,35 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class EmptyWidget extends StatelessWidget {
+  final Size size;
+  final EdgeInsets viewPadding;
   final String svgAsset;
   final String message;
+  final Future<void> Function() onRefresh;
   const EmptyWidget({
     required this.svgAsset,
     required this.message,
+    required this.size,
+    required this.viewPadding,
+    required this.onRefresh,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Flexible(
-          child: SvgPicture.asset(svgAsset),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Flexible(
-          child: Text(
-            message,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-            ),
+    return RefreshIndicator(
+      onRefresh: onRefresh,
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: SizedBox(
+          width: size.width,
+          height: size.height - AppBar().preferredSize.height - viewPadding.top,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: SvgPicture.asset(
+                  svgAsset,
+                  height: size.height * .3,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Flexible(
+                child: Text(
+                  message,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
--- lib/widget/errorwidget.dart
+++ lib/widget/errorwidget.dart
@@ -4,33 +4,54 @@ import 'package:flutter_svg/flutter_svg.dart';
 class CustomErrorWidget extends StatelessWidget {
   final String svgAsset;
   final String message;
+  final Size size;
+  final EdgeInsets viewPadding;
+  final Future<void> Function() onRefresh;
+
   const CustomErrorWidget({
     required this.svgAsset,
     required this.message,
+    required this.size,
+    required this.viewPadding,
+    required this.onRefresh,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Flexible(
-          child: SvgPicture.asset(svgAsset),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Flexible(
-          child: Text(
-            message,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-            ),
+    return RefreshIndicator(
+      onRefresh: onRefresh,
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: SizedBox(
+          width: size.width,
+          height: size.height - AppBar().preferredSize.height - viewPadding.top,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: SvgPicture.asset(
+                  svgAsset,
+                  height: size.height * .3,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Flexible(
+                child: Text(
+                  message,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
