45dd430504667496fc3a323df380c4fab226ffd4
Emon526 <emonats526@gmail.com>
2023-04-29 04:17:59 +0600

    updated

--- lib/services/notificationservice.dart
+++ lib/services/notificationservice.dart
@@ -46,7 +46,7 @@ class NotificationService {
         tz.local,
       ),
       await notificationDetails(),
-      androidAllowWhileIdle: true,
+      // androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
       payload: payload,
       uiLocalNotificationDateInterpretation:
           UILocalNotificationDateInterpretation.absoluteTime,
