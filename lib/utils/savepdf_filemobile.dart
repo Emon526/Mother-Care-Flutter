3bf655f160a8b860644e5c862e1dfa7d8277cdaf
Emon526 <emonats526@gmail.com>
2023-05-10 22:12:46 +0600

    updated

--- lib/utils/savepdf_filemobile.dart
+++ lib/utils/savepdf_filemobile.dart
@@ -1,9 +1,8 @@
-import 'dart:io';
+// ignore_for_file: depend_on_referenced_packages
 
-// import 'package:open_file/open_file.dart' as open_file;
+import 'dart:io';
 import 'package:better_open_file/better_open_file.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
-// ignore: depend_on_referenced_packages
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 
 ///To save the pdf file in the device
