8225f2d13ac7cab08a08d57c249948f54e5c647a
Emon526 <emonats526@gmail.com>
2023-05-12 02:10:59 +0600

    language applied

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -88,7 +88,7 @@ class SettingScreen extends StatelessWidget {
             Positioned(
               // bottom: 20,
               child: Text(
-                Consts.APP_NAME,
+                AppLocalizations.of(context)!.appname,
                 style: TextStyle(
                   fontSize: 24.0,
                   color: Theme.of(context).primaryColor,
