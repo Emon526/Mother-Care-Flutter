082a2ae2d20dc5dee828bf1c6840148633f54ffa
Emon526 <emonats526@gmail.com>
2023-06-20 05:28:02 +0600

    custom loading added

--- lib/screens/auth/signup.dart
+++ lib/screens/auth/signup.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
