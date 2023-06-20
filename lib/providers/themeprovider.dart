b6fc07b5bc8f1b3b46bcb02d09efbc867dba02ff
Emon526 <emonats526@gmail.com>
2023-06-21 03:15:28 +0600

    theme mode updated

--- lib/providers/themeprovider.dart
+++ lib/providers/themeprovider.dart
@@ -9,7 +9,7 @@ class ThemeProvider with ChangeNotifier {
   bool _isDarkTheme = false;
   bool get isDarkTheme => _isDarkTheme;
 
-  ThemeMode _themeMode = ThemeMode.system;
+  ThemeMode _themeMode = ThemeMode.light;
   ThemeMode get themeMode => _themeMode;
 
   set themeMode(ThemeMode themeMode) {
